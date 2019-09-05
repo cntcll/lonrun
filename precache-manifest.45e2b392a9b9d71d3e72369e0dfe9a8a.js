19041a297522312b530168be8ddb68cff107ca04
INDRIVETEC\f.conticelli <f.conticelli@indrivetec.ch>
2019-09-05 16:12:57 +0200

    Updates

---
+++ precache-manifest.45e2b392a9b9d71d3e72369e0dfe9a8a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc9f2dbb30a4d23dc0a50d2011c15405",
+    "url": "/longrun/index.html"
+  },
+  {
+    "revision": "2bb757e77495f5ebd287",
+    "url": "/longrun/static/css/main.bf286928.chunk.css"
+  },
+  {
+    "revision": "81abc5da3acc1cd5248d",
+    "url": "/longrun/static/js/2.37570b65.chunk.js"
+  },
+  {
+    "revision": "2bb757e77495f5ebd287",
+    "url": "/longrun/static/js/main.00faf3a7.chunk.js"
+  },
+  {
+    "revision": "9f17ffaea37e007f5bb2",
+    "url": "/longrun/static/js/runtime~main.40d006f5.js"
+  }
+]);
